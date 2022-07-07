547be4abb3e173caffd6c6a8d2f01f77822480bf
Osinyomi Adeola <95629502+Osinyomi@users.noreply.github.com>
2022-07-07 17:59:43 +0000

    i add a couple of codes to the function to make it function and execute

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers){
+   let sum = 0;
+    for(i = 0; i < arrayOfNumbers.length; i++) {
+     sum += arrayOfNumbers[i]
+    }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ sumOfNumbers([2,4,3,4,5,1])
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,11 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let EvenNumber  = arrayOfNumbers.filter(number => {return number  % 2 === 0;});
+  
+    return EvenNumber.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
